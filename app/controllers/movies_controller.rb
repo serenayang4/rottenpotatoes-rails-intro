f8d967818d453f4a4fba2f3a71b32601c3f20b97
EC2 Default User <ec2-user@ip-172-31-34-167.ec2.internal>
2020-09-15 22:25:16 +0000

    part 2 troubleshoot

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,7 @@ class MoviesController < ApplicationController
       ratings = @ratings.keys
       session[:ratings] = @ratings
     else #no checked
-      if(!params.has_key?(:commit)) #not submitted yet
+      if(!params.has_key?(:commit) && !params.has_key?(:sort)) #not submitted yet
         ratings = Movie.all_ratings.keys
         session[:ratings] = Movie.all_ratings
       else #was submitted
